b895cae83e0ca2cb7612c3f208b3bf73bb0e78ca
rngrhn4114 <130418820+rngrhn4114@users.noreply.github.com>
2024-01-13 17:18:42 +0900

    Update KioskMain.java

--- KioskMain.java
+++ KioskMain.java
@@ -1,14 +1,9 @@
 package oop_kiosk_medihub;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.Panel;
-import java.awt.Button;
-import java.awt.Label;
 
 public class KioskMain extends JFrame {
 	public JFrame frame;
@@ -31,7 +26,7 @@ public class KioskMain extends JFrame {
     	getContentPane().setLayout(null);
     	
     	/* 접수 버튼 생성 */
-    	Button btnrg = new Button("접수");
+    	JButton btnrg = new JButton("접수");
     	btnrg.setForeground(Color.WHITE);
     	btnrg.setFont(new Font("맑은 고딕", Font.BOLD, 65));
     	btnrg.setBackground(new Color(32, 178, 170));
@@ -50,7 +45,7 @@ public class KioskMain extends JFrame {
     	getContentPane().add(btnrg);
     	
     	/* 수납 버튼 생성 */
-    	Button btnpay = new Button("수납");
+    	JButton btnpay = new JButton("수납");
     	btnpay.setForeground(Color.WHITE);
     	btnpay.setFont(new Font("맑은 고딕", Font.BOLD, 65));
     	btnpay.setBackground(new Color(32, 178, 170));
@@ -69,10 +64,10 @@ public class KioskMain extends JFrame {
 		getContentPane().add(btnpay);
     	
     	/* 관리자모드 버튼 생성 */
-    	Button btnAdmin = new Button("관리자 모드");
-    	btnAdmin.setForeground(Color.WHITE);
+    	JButton btnAdmin = new JButton("관리자 모드");
+    	btnAdmin.setForeground(Color.BLACK);
     	btnAdmin.setFont(new Font("맑은 고딕", Font.BOLD, 25));
-    	btnAdmin.setBackground(new Color(32, 178, 170));
+    	btnAdmin.setBackground(new Color(255, 255, 255));
     	btnAdmin.setBounds(1200, 20, 200, 50);
     	getContentPane().add(btnAdmin);
     	
@@ -85,19 +80,19 @@ public class KioskMain extends JFrame {
     		}
     	});
     	
-    	Label Medihub = new Label("Medihub");
+    	JLabel Medihub = new JLabel("Medihub");
     	Medihub.setForeground(new Color(32, 178, 170));
     	Medihub.setFont(new Font("맑은 고딕", Font.BOLD, 50));
     	Medihub.setBounds(60, 30, 400, 50);
     	getContentPane().add(Medihub);
     	
-    	Label greetings = new Label("안녕하세요. MediHub입니다.");
+    	JLabel greetings = new JLabel("안녕하세요. MediHub입니다.");
     	greetings.setForeground(Color.BLACK);
     	greetings.setFont(new Font("맑은 고딕", Font.BOLD, 20));
     	greetings.setBounds(60, 100, 800, 50);
     	getContentPane().add(greetings);
     	
-    	Label selecting = new Label("원하시는 항목을 선택해주십시오.");
+    	JLabel selecting = new JLabel("원하시는 항목을 선택해주십시오.");
     	selecting.setForeground(Color.BLACK);
     	selecting.setFont(new Font("맑은 고딕", Font.BOLD, 30));
     	selecting.setBounds(300, 200, 800, 50);
