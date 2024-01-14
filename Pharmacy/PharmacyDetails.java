bf5087c2831c161f11a378f0c9eac42092464a39
jieun1102 <55270989+jieun1102@users.noreply.github.com>
2024-01-14 19:29:02 +0900

    Update PharmacyDetails.java

---
+++ Pharmacy/PharmacyDetails.java
@@ -0,0 +1,46 @@
+package pharmacy;
+
+//약국의 세부정보를 나타냄
+//약국명, 전화번호, 주소, 취급약품, 거리, 평점을 저장하는 class
+public class PharmacyDetails {
+	private String pharmacy;
+	private String callNumber;
+	private String address;
+	private String medicine;
+	private int distance;
+	private String star;
+
+	public PharmacyDetails(String pharmacy, String callNumber, String address, String medicine, int distance, String star) {
+
+		this.pharmacy = pharmacy;
+		this.callNumber = callNumber;
+		this.address = address;
+		this.medicine = medicine;
+		this.distance = distance;
+		this.star = star;
+	}
+ 
+	 public String getStar() {
+	     return star;
+	 }
+
+	 public String getPharmacy() {
+	     return pharmacy;
+	 }
+	
+	 public String getCallNumber() {
+	     return callNumber;
+	 }
+	
+	 public String getAddress() {
+	     return address;
+	 }
+	
+	 public String getMedicine() {
+	     return medicine;
+	 }
+	
+	 public double getDistance() {
+	     return distance;
+	 }
+}
